5da498c0c3bea5c23c6af07f9d0321638bf40b4b
Martin Prinz <martin.prinz@maibornwolff.de>
2022-05-13 13:22:32 +0000

    feat(token-transform): remove helper fns for references

--- scripts/utils.js
+++ scripts/utils.js
@@ -28,46 +28,10 @@ function createArray({ dictionary }) {
   return JSON.stringify(arr);
 }
 
-/**
- * Check whether a token value is a reference to another
- * Currently this reference check is based on convention (starts with $)
- * @param {*} value token value
- * @returns boolean
- */
-function isReference(value) {
-  if (!value || typeof value === 'object') {
-    return false;
-  }
-  return value.substr(0, 1) === '$';
-}
-
-/**
- * Resolves token references based on its path and category
- * @param {*} tokens all tokens
- * @param {*} value reference to another token value, e.g. $magenta.500
- * @returns token value
- */
-function getReferenceValue(tokens, value) {
-  const parts = value.substr(1, value.length - 1).split('.');
-
-  // support references starting with or without category
-  // (e.g. $colors.magenta.500 and $magenta.500)
-  const { category } = tokens[0].attributes;
-  if (!parts.includes(category)) {
-    parts.push(category);
-  }
-  const res = tokens.find(
-    (t) => t.path.sort().toString() === parts.sort().toString(),
-  );
-  const v = typeof res === 'object' ? res.value : res;
-  return isReference(v) ? getReferenceValue(tokens, v) : v;
-}
-
 function filterTokensByType(type, tokens) {
   const obj = tokens.reduce((acc, cur) => {
     if (cur.type === type) {
-      const value = isReference(cur.value) ? getReferenceValue(tokens, cur.value) : cur.value;
-      acc[cur.path.join('.')] = `var(--${cur.name}, ${value})`;
+      acc[cur.path.join('.')] = `var(--${cur.name}, ${cur.value})`;
     }
     return acc;
   }, {});
@@ -77,5 +41,5 @@ function filterTokensByType(type, tokens) {
 }
 
 module.exports = {
-  createArray, filterTokensByType, isReference, getReferenceValue,
+  createArray, filterTokensByType,
 };
