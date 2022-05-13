b1ed8b340998c3c7dd450beb140cf0b7b78174e2
Martin Prinz <martin.prinz@maibornwolff.de>
2022-05-13 14:45:44 +0000

    skip css generation

--- index.js
+++ index.js
@@ -3,34 +3,34 @@
 const tailwindConfig = require('./dist/tailwind/tailwind.config.js');
 
 // global
-const css_global = require('./dist/global.css');
+// const css_global = require('./dist/global.css');
 const scss_global = require('./dist/global.scss');
 const json_global = require('./dist/global.json');
 
 // mw-theme (light)
-const css_light = require('./dist/mw-theme.css');
+// const css_light = require('./dist/mw-theme.css');
 const scss_light = require('./dist/mw-theme.scss');
 const json_light = require('./dist/mw-theme.json');
 
 // mw-theme_dark
-const css_dark = require('./dist/mw-theme_dark.css');
+// const css_dark = require('./dist/mw-theme_dark.css');
 const scss_dark = require('./dist/mw-theme_dark.scss');
 const json_dark = require('./dist/mw-theme_dark.json');
 
 module.exports = {
   tailwindConfig,
   global: {
-    css: css_global,
+    // css: css_global,
     scss: scss_global,
     json: json_global,
   },
   light: {
-    css: css_light,
+    // css: css_light,
     scss: scss_light,
     json: json_light,
   },
   dark: {
-    css: css_dark,
+    // css: css_dark,
     scss: scss_dark,
     json: json_dark,
   },
