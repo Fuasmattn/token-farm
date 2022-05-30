68aac6e526833555f3797862f46a841ec979abea
Martin Prinz <martin.prinz@maibornwolff.de>
2022-05-30 20:46:53 +0000

    remove example tailwind

--- dist/js/mw-theme_dark.js
+++ dist/js/mw-theme_dark.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
+ * Generated on Mon, 30 May 2022 20:46:15 GMT
+ */
 
- * Generated on Mon, 30 May 2022 20:35:41 GMT */
-
-export const fgDefault = '#d3126c';
+export const fgDefault = "#d3126c";
