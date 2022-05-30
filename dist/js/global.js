67112e5925618ae0c08ec261379387b4b26b8c21
Fuasmattn <Fuasmattn@users.noreply.github.com>
2022-05-30 21:07:00 +0000

    Update Tokens

--- dist/js/global.js
+++ dist/js/global.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Mon, 30 May 2022 21:04:55 GMT
+ * Generated on Mon, 30 May 2022 21:06:58 GMT
  */
 
 export const magenta100 = "#fde8f2";
--- dist/js/mw-theme.js
+++ dist/js/mw-theme.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Mon, 30 May 2022 21:04:55 GMT
+ * Generated on Mon, 30 May 2022 21:06:58 GMT
  */
 
 export const semanticColorActionsFilledDefaultBgColor = "#ed2c86";
